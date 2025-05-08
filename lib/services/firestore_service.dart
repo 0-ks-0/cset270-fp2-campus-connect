c39a2aa011c156166955ae74e665d3e0647b9673
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2025-05-08 16:12:56 -0400

    Use post collection reference variable

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -2,17 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService
 {
+  var postsCollection = FirebaseFirestore.instance.collection("posts");
+
   /// Creates a post
   void createPost(String title, String content, DateTime timestamp, String author)
   {
-    FirebaseFirestore
-      .instance
-      .collection("posts")
-      .add({
-        "title": title,
-        "content": content,
-        "timestamp":  Timestamp.fromDate(timestamp),
-        "author": author
+    postsCollection.add({
+      "title": title,
+      "content": content,
+      "timestamp":  Timestamp.fromDate(timestamp),
+      "author": author
     });
   }
 }
