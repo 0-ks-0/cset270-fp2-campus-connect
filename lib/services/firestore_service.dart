154a5204026165c2e10948b00076b865cfbd4d1d
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2025-05-08 14:51:18 -0400

    Add creating a post

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireStoreService
+{
+  /// Creates a post
+  void createPost(String title, String description, DateTime timestamp, String author)
+  {
+    FirebaseFirestore
+      .instance
+      .collection("posts")
+      .add({
+        "title": title,
+        "description": description,
+        "timestamp":  Timestamp.fromDate(timestamp),
+        "author": author
+    });
+  }
+}
