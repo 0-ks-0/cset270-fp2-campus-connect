082554e0e006718819b51fcdcfb1d385dd066d3d
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2025-05-08 16:24:36 -0400

    Allow null timestamp argument for createPost
    Default to current timestamp if null

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -5,12 +5,12 @@ class FirestoreService
   var postsCollection = FirebaseFirestore.instance.collection("posts");
 
   /// Creates a post
-  void createPost(String title, String content, DateTime timestamp, String author)
+  void createPost(String title, String content, String author, DateTime? timestamp)
   {
     postsCollection.add({
       "title": title,
       "content": content,
-      "timestamp":  Timestamp.fromDate(timestamp),
+      "timestamp":  timestamp == null ? Timestamp.now() : Timestamp.fromDate(timestamp),
       "author": author
     });
   }
