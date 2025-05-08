4d304018d2589b3e9e71856e5de2fd27cc630d1f
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2025-05-08 16:34:54 -0400

    Move FirestoreService instance and convert to stateful widget

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -2,9 +2,17 @@ import "package:flutter/material.dart";
 
 import "package:campus_connect/services/firestore_service.dart";
 
-class Test extends StatelessWidget
+class Test extends StatefulWidget
 {
-  const Test({super.key});
+  Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test>
+{
+  FirestoreService fs = FirestoreService();
 
   @override
   Widget build(BuildContext context)
@@ -13,7 +21,6 @@ class Test extends StatelessWidget
       floatingActionButton: FloatingActionButton(
         onPressed: ()
         {
-          FirestoreService fs = FirestoreService();
           fs.createPost("code test", "created through code", "k", null);
           fs.createPost("code test 2", "created through code", "k", DateTime.now());
         },
