06e95cee54b3c347cb77cc5626ab6e8ffafee9e5
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2025-05-08 13:18:16 -0400

    Bring back firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import "package:campus_connect/screens/welcome_page.dart";
 
-void main()
+void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
